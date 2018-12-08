17e00e8a8dce3c0e18caf31db46a3444ac33c0af
orion6912 <orion6912@gmail.com>
2018-12-08 17:11:31 +0900

    circle ci 用設定修正 deploy設定修正 goのmodel修正

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -6,12 +6,23 @@ echo 'deploy start...' | /home/ec2-user/isucon8-final/shell/slack.sh
 
 # git
 cd /home/ec2-user/isucon8-final
+echo 'git pull start...' | /home/ec2-user/isucon8-final/shell/slack.sh 
 git pull
+echo 'git pull end...' | /home/ec2-user/isucon8-final/shell/slack.sh 
 
 # docker
+echo 'docker-compose start...' | /home/ec2-user/isucon8-final/shell/slack.sh 
 docker-compose -f /home/ec2-user/isucon8-final/webapp/docker-compose.yml -f /home/ec2-user/isucon8-final/webapp/docker-compose.go.yml down
 # docker-compose -f /home/ec2-user/isucon8-final/webapp/docker-compose.yml -f /home/ec2-user/isucon8-final/webapp/docker-compose.go.yml build
 docker-compose -f /home/ec2-user/isucon8-final/webapp/docker-compose.yml -f /home/ec2-user/isucon8-final/webapp/docker-compose.go.yml up -d
+echo 'docker-compose end...' | /home/ec2-user/isucon8-final/shell/slack.sh 
 
+
+echo 'bench make start...' | /home/ec2-user/isucon8-final/shell/slack.sh
+cd /home/ec2-user/isucon8-final/bench
+make init
+make deps
+make build
+echo 'bench make end...' | /home/ec2-user/isucon8-final/shell/slack.sh 
 
 echo 'deploy finish!!!' | /home/ec2-user/isucon8-final/shell/slack.sh 
